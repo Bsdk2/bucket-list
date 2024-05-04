726e80d0bcb08cbb8b3947670ab3099bbe45f307
pycho <149138256+Bsdk2@users.noreply.github.com>
2024-05-04 14:38:14 +0530

    updated few things

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
+// remove
+// mark
+
 const listitem = document.getElementById("uli")
 const butto = document.getElementById('butto')
 const inputext = document.getElementById('textarea')
 
+
 butto.addEventListener('click', function () {
     const newinput = inputext.value.trim()
-    const new_elem = document.createElement('li')
-    new_elem.textContent = newinput
-    listitem.appendChild(new_elem)
-    newinput.value = ''
+    if (newinput === '') {
+        inputext.placeholder = 'add some text'
+    }
+    else {
+        const new_elem = document.createElement('li')
+        new_elem.textContent = newinput
+        listitem.appendChild(new_elem)
+        inputext.value = ''
+    }
+
 })
+const child = document.querySelector("ul>li")
+function handledoubleclick(event) {
+    if (event.target.child = 'li') {
+        console.log('CLICKED ON NEW LI')
+    }
+    child.addEventListener('dblclick', function () {
+        console.log('child');
+    }
+
+
+
+    )
+}
